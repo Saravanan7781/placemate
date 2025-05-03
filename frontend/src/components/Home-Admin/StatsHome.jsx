e45d2888a53dfe07ed5c166f316e7b0a57a9162a
Saravanan7781 <saravanansomasundharamk@gmail.com>
2025-05-03 13:24:36 +0530

    Started and done half work of dashboard - admin

---
+++ frontend/src/components/Home-Admin/StatsHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './StatsHome.css'
+
+function StatsHome() {
+  return (
+    <div className="StatsHomeMain">
+      StatsHome
+    </div>
+  )
+}
+
+export default StatsHome
