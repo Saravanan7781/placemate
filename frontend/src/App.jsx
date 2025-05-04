e72249d5b2c814e3ca6feb7c4ba811539487c46a
moneshvel66 <moneshvel66@gmail.com>
2025-05-04 18:49:14 +0530

    Landing page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import { Routes,Route,BrowserRouter as Router} from 'react-router-dom'
 import RegisterCompany from './components/RegisterCompany/RegisterCompany'
 import Home from './pages/Home'
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <div className="mainApp">
     <Router>
         <Routes>
-          <Route path="/registerCompany" element={ <RegisterCompany />}>
-          </Route>
-          <Route path="/home" element={<Home/> }>
-
-          </Route>
+          <Route path="/registerCompany" element={ <RegisterCompany />}></Route>
+          <Route path="/home" element={<Home/> }></Route>
+          <Route path="" element={<LandingPage/> }></Route>
          </Routes>
     </Router>
     </div>
---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import './LandingPages.css'
+
+function LandingPage(){
+
+    return(
+        <h1>Landing Page</h1>
+    )
+}
+
+export default LandingPage
