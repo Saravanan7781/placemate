0f51fec2cc7d647b12e548aa456afbd25889debf
Saravanan7781 <saravanansomasundharamk@gmail.com>
2025-05-06 00:06:57 +0530

    Created sidebar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,18 @@ import RegisterCompany from './components/RegisterCompany/RegisterCompany';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import LandingPage from './pages/LandingPage';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="mainApp">
       <Router>
+        <Sidebar/>
         <Routes>
           <Route path="/registerCompany" element={<RegisterCompany />}></Route>
           <Route path="/home" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/" element={<LandingPage/> }></Route>
+          <Route path="/" element={<LandingPage />}></Route>
          </Routes>
     </Router>
     </div>
--- frontend/src/components/Home-Admin/RealTimeDashboard.jsx
+++ frontend/src/components/Home-Admin/RealTimeDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './RealTimeDashboard.css';
 import { CalendarCheck, CalendarClock, NotepadText } from 'lucide-react';
+import totalStudents from '../../assets/total_students.png';
 
 function RealTimeDashboard() {
   const data = [
@@ -8,11 +9,17 @@ function RealTimeDashboard() {
       title: 'Total Students',
       number: 100,
       icon: NotepadText,
+      backgroundImage: totalStudents,
+      beforeGradient:
+        'linear-gradient(143deg, rgba(198, 213, 255, 1) 0%, rgba(243, 207, 255, 1) 43%) ',
     },
     {
       title: 'Students placed',
       number: 70,
       icon: CalendarCheck,
+      beforeGradient:
+        'linear-gradient(167deg, rgba(153, 235, 255, 1) 33%, rgba(103, 192, 214, 1) 81%)',
+      // ,
     },
     {
       title: 'Yet to be placed',
@@ -26,13 +33,20 @@ function RealTimeDashboard() {
       {data.map((data, index) => {
         const Icon = data.icon;
         return (
-          <div className="RealTimeDashboardRuntime" key={index}>
+          <div
+            className="RealTimeDashboardRuntime"
+            key={index}
+            style={{
+              background: data.beforeGradient,
+              transition:'all 0.5s'
+            }}
+          >
             <div className="RealTimeDashboardRuntimeHeading">
               <p>{data.title}</p>
               <Icon />
             </div>
             <div className="RealTimeDashboardRuntimeBackground">
-              <p>image</p>
+              <img src={data.backgroundImage} alt="" />
             </div>
             <div className="RealTimeDashboardRuntimeNumber">
               <p>{data.number}</p>
