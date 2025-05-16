6de96ed7d6bb4e48d171765925fce16a1ffb3d64
Saravanan7781 <saravanansomasundharamk@gmail.com>
2025-05-16 23:05:00 +0530

    Update: User profile page

--- frontend/src/components/Profile/ProfileDivOne.jsx
+++ frontend/src/components/Profile/ProfileDivOne.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './ProfileDivOne.css';
+import image from '../../assets/SampleUserImages/saran.png';
 
 function ProfileDivOne() {
   return (
     <>
       <div className="profileDiv1MainPicture">
-        <img src="" alt="user_pic" />
+        <img src={image} alt="user_pic" />
       </div>
       <div className="profileDiv1Data">
         <div className="profileDiv1DataName profileDiv1Datum">
@@ -22,7 +23,31 @@ function ProfileDivOne() {
         </div>
         <div className="profileDiv1DataDepartment profileDiv1Datum">
           <p className="profileDiv1DataQues">Department</p>
-          <p className="profileDiv1DataAns">Computer Science and Business Systems</p>
+          <p className="profileDiv1DataAns">
+            Computer Science and Business Systems
+          </p>
+        </div>
+        <div className="profileDiv1DataSchoolMarks profileDiv1Datum">
+          <div className="profileDiv1Data10thSchoolMark">
+            <p className="profileDiv1Data10thSchoolMarkQues ">10th</p>
+            <p className="profileDiv1Data10thSchoolMarkAns">90%</p>
+          </div>
+          <div className="profileDiv1Data12thSchoolMark">
+            <p className="profileDiv1Data12thSchoolMarkQues">12th</p>
+            <p className="profileDiv1Data12thSchoolMarkAns">94% </p>
+          </div>
+        </div>
+        <div className="profileDiv1DataArrears profileDiv1Datum">
+          <div className="profileDiv1DataCurrentArrear">
+            <p className="profileDiv1DataCurrentArrearQues ">Current Arrears</p>
+            <p className="profileDiv1DataCurrentArrearAns">1</p>
+          </div>
+          <div className="profileDiv1DataHistoryOfArrears">
+            <p className="profileDiv1DataHistoryOfArrearsQues">
+              History of Arrears
+            </p>
+            <p className="profileDiv1DataHistoryOfArrearsAns">2 </p>
+          </div>
         </div>
       </div>
     </>
